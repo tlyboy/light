4e8034c82e8b065545efaa68e6d4cd0947d90ec6
Guany <1084072690@qq.com>
2022-04-29 20:23:31 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-let box = document.querySelector('#box')
+let light = document.querySelector('#light')
 
 let s = false
-box.addEventListener('click', e => {
+
+light.addEventListener('click', e => {
   if (s == false) {
-    e.target.className = 'box turn-on'
+    e.target.className = 'light turn-on'
     e.target.innerText = '关灯'
     document.body.style.backgroundColor = '#ecf0f1'
     s = true
   } else {
-    e.target.className = 'box turn-off'
+    e.target.className = 'light turn-off'
     e.target.innerText = '开灯'
     document.body.style.backgroundColor = '#2c3e50'
     s = false
